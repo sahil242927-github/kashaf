4c0c49d7a0f9585724f9604e4bd6af1071f6a307
Mohd Sahil <sahil242927@gmail.com>
2020-06-10 09:41:05 +0530

    Updates

---
+++ precache-manifest.d21d6be9fbbb9cdcc51b2190fe7086fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804f3f7c99636b10d4052974d8421532",
+    "url": "/kashaf/index.html"
+  },
+  {
+    "revision": "837aee83a06b7153ef8e",
+    "url": "/kashaf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "195180385247bac96fb9",
+    "url": "/kashaf/static/js/2.2993dbbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kashaf/static/js/2.2993dbbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "837aee83a06b7153ef8e",
+    "url": "/kashaf/static/js/main.f8f45ca3.chunk.js"
+  },
+  {
+    "revision": "955d2ac4d57b36ee9b10",
+    "url": "/kashaf/static/js/runtime-main.1b5d69bc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kashaf/static/media/logo.5d5d9eef.svg"
+  }
+]);
